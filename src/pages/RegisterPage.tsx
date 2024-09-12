4f418f73611c1920345311bca16c8acb81e61e51
mustafaduyarer <mustafaduyarer@gmail.com>
2024-09-12 23:47:03 +0200

    registerPage created

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../css/RegisterPage.css"
+
+function RegisterPage() {
+  return (
+    <div className='register'>
+      <div>
+        <form>
+          <div>
+
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default RegisterPage
